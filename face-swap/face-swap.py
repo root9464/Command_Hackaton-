59cdada51429442dba632c1c48fead0f0357095c
iO <senpai9464@yandex.ru>
2024-04-21 20:06:04 +0500

    finish

---
+++ face-swap/face-swap.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import sys
+import cv2
+
+from face_swaps.face_swap import swap_faces
+
+
+if __name__ == '__main__':
+
+    (major_ver, minor_ver, subminor_ver) = cv2.__version__.split('.')
+
+    if int(major_ver) < 3:
+        print >> sys.stderr, 'ERROR: Script needs OpenCV 3.0 or higher'
+        sys.exit(1)
+
+    print(sys.argv)
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+
+    img1 = cv2.imread(filename1)
+    img2 = cv2.imread(filename2)
+
+    output = swap_faces(img1, img2)
+
+    cv2.imwrite('result.jpg', output)
+
+    cv2.imshow("Face Swapped", output)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
